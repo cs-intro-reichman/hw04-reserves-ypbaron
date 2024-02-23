70a02963aba01faa27d5652cd1e613fbe3eadffd
Yuval Baron <yuval.baron@post.runi.ac.il>
2024-02-23 17:58:40 +0200

    HW$

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,26 +1,58 @@
 public class ArrayOps {
     public static void main(String[] args) {
-        
+   
     }
     
     public static int findMissingInt (int [] array) {
-        // Write your code here:
+		int[] sortedArr = sortArr(array);
+		for(int i = 0 ; i < array.length ; i++){
+			if(sortedArr[i] != i){
+				return i;
+			}
+		}
         return -1;
     }
 
+
     public static int secondMaxValue(int [] array) {
-        // Write your code here:
-        return 0;
+		int[] sortedArr = sortArr(array);
+		return sortedArr[sortedArr.length - 2];
     }
 
     public static boolean containsTheSameElements(int [] array1,int [] array2) {
-        // Write your code here:
-        return false;
+        int[] sortArray1 = sortArr(array1);
+		int[] sortArray2 = sortArr(array2); ///need to continue from here... maybe create a new array where you delete duplicates
+		
+		for (int i = 0; i <= length.sortArray1; i++) {
+			
+		}
+		
+        return true;
     }
 
     public static boolean isSorted(int [] array) {
-        // Write your code here:
-        return false;
+		int[] sortedArr = sortArr(array);
+		for(int i = 0; i < array.length ; i++){
+			if (sortedArr[i] != array[i]){
+				return false;
+			}	
+		}
+        return true;
     }
+				
+	public static int[] sortArr(int[] inputArr){
+		// temp is for swapping elements when needed.
+		int temp;
+		for(int i = 0; i < inputArr.length; i++){
+			for(int j = 0; j < inputArr.length - 1 - i; j++){
+				if(inputArr[j] > inputArr[j + 1]){
+					temp = inputArr[j + 1];
+					inputArr[j + 1] = inputArr[j];
+					inputArr[j] = temp;
+				}
+			}
+		}
+		return inputArr; // supposed to work but there may be a problem
+	}
 
 }
--- StringOps.java
+++ StringOps.java
@@ -1,3 +1,6 @@
+import java.sql.Array;
+import java.util.ArrayList;
+
 public class StringOps {
     ////////////////////////////////////////////////////////////
     //////                                               ///////
@@ -22,21 +25,61 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
+		String s = args[0];
+		String voweResult = capVowelsLowRest(s);
+		System.out.print(voweResult);   
     }
 
     public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
-    }
+		String ans = "";
+		for (int i = 0; i < string.length(); i++) {
+			char ch = string.charAt(i);
+			if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+				ch = (char) (string.charAt(i) - 32); // returns uppers case vowels
+				ans+= ch;
+			} else {
+				if ((ch >= 65) && (ch <= 90)){
+					ch = (char)(string.charAt(i) + 32); // returns lower case
+					ans += ch;
+				} else {
+				ans += ch;
+				}
+			}
+		}	
+		return ans; // understand why it doesnt bring back spaces
+	}
 
-    public static String camelCase (String string) {
-        // Write your code here:
-        return "";
-    }
+
+    public static String camelCase (String string) { // figure this code out
+        String strcamelCase = args[0];
+		String improved = "" + (char)(str.charAt(0) - 32);
+		int j = 1;
+		for (int i = 0; i < string.length() && string.charAt(i) != ' '; i++){ // indicates first words 
+			char chFirstWord = string.strcamelCase(i);
+			firstWord = (char)(string.charAt(i) + 32);	// returns all the letters in first word lower case	- check to add print
+			firstWord++;			
+		}
+		while (j >= indexOf(str.charAt(i)) && j < str.length()) { // brings back uppercase letter for first letter without spaces
+			char ch = str.charAt(j);
+			if (ch == ' '){
+				improved = chFirstWord + (char) (str.charAt(j + 1) - 32); 
+				j++;
+			} else {
+				improved++;
+			}
+		    return firstWord + improved;
+		}
+	}
+
 
     public static int[] allIndexOf (String string, char chr) {
-        // Write your code here:
-        return new int[1];
+		ArrayList indexes = new ArrayList<>();
+		for (int i = 0; i < string.length(); i++) {
+			if (chr == string.charAt(i)){
+				indexes.add(i);
+			}
+		}
+        return indexes;
     }
 }
+
